b7637cca7461c084acb17a83e7406a56a30fd4e0
Araki_Takashi <ellaydelgado@gmail.com>
2025-09-09 06:13:34 +0900

    iamのtfファイルを修正

--- infra/iam/providers.tf
+++ infra/iam/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
   backend "s3" {
     bucket = "terraform-state-taraki"
-    key    = "service/terraform.tfstate"
+    key    = "iam/terraform.tfstate"
     region = "ap-northeast-1"
   }
   required_providers {
